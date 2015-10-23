5a11ca878abfe0d036dfbed24d268dc78e40a4d7
iamrt <rance@ranceturley.com>
2015-10-23 11:13:41 -0500

    Issue with mobile menu fixed. Need to Fix UL as it toggles

--- js/main.js
+++ js/main.js
@@ -1,21 +1,39 @@
 $(function() {
 	
+/***************Mobile Menu***************/
+
 	$('#mobile-menu').on('click', function() {
-		$('nav ul').toggleClass('open')
+		$('nav > ul').slideToggle(600)
 		$('button').toggleClass('change-bg');
 	});
 
+/***************Gallery View***************/
+
+	$extView = $('#ext-view');
+	$intView = $('#int-view');
+
+	$('.ext-View').on('click', function() {
+
+		$id = $(this).attr('id');
+
+	if ($extView.css('display') == ('block') && ($extView.html().contains($id))) {
+			$extView.slideToggle(500);
+		};
+	});
+
 	$('.ext-View').on('click', function () {
-		$extView = $('#ext-view');
-		$intView = $('#int-view');
+
+		$id = $(this).attr('id');
 
 		if ($extView.css('display') == ('none')) {
 			$extView.slideToggle(500);
-			$extView.html('<div class=\"showcase-bf\"><h3>Before</h3><img src=img/' + $(this).attr('id') + '-bf.jpg></div><div class=\"showcase-af\"><h3>After</h3><img src=img/' + $(this).attr('id') + '-af.jpg></div>');
+			$extView.html('<div class=\"showcase-bf\"><h3>Before</h3><img src=img/' + $id + '-bf.jpg></div><div class=\"showcase-af\"><h3>After</h3><img src=img/' + $id + '-af.jpg></div>');
 	
 		}	else	{		
-			$extView.html('<div class=\"showcase-bf\"><h3>Before</h3><img src=img/' + $(this).attr('id') + '-bf.jpg></div><div class=\"showcase-af\"><h3>After</h3><img src=img/' + $(this).attr('id') + '-af.jpg></div>');
+			$extView.html('<div class=\"showcase-bf\"><h3>Before</h3><img src=img/' + $id + '-bf.jpg></div><div class=\"showcase-af\"><h3>After</h3><img src=img/' + $id + '-af.jpg></div>');
 		};
 	});
+
+	
 });
 
