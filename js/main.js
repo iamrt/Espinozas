b4872751d148f92a1b191135650b3fc901ac4871
iamrt <rance@ranceturley.com>
2015-10-23 09:35:05 -0500

    Fixed gallery and Showcase div. May play with styling.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 $(function() {
+	
 	$('#mobile-menu').on('click', function() {
 		$('nav ul').toggleClass('open')
 		$('button').toggleClass('change-bg');
 	});
 
+	$('.ext-View').on('click', function () {
+		$extView = $('#ext-view');
+		$intView = $('#int-view');
+
+		if ($extView.css('display') == ('none')) {
+			$extView.slideToggle(500);
+			$extView.html('<div class=\"showcase-bf\"><h3>Before</h3><img src=img/' + $(this).attr('id') + '-bf.jpg></div><div class=\"showcase-af\"><h3>After</h3><img src=img/' + $(this).attr('id') + '-af.jpg></div>');
+	
+		}	else	{		
+			$extView.html('<div class=\"showcase-bf\"><h3>Before</h3><img src=img/' + $(this).attr('id') + '-bf.jpg></div><div class=\"showcase-af\"><h3>After</h3><img src=img/' + $(this).attr('id') + '-af.jpg></div>');
+		};
+	});
 });
 
