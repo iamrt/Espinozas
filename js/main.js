3129ad67b5cc04f89c3716800949b878d25d6289
iamrt <rance@ranceturley.com>
2015-11-22 13:55:33 -0600

    added more pics to gallery, and about me

--- js/main.js
+++ js/main.js
@@ -11,8 +11,15 @@ $(function() {
 
 	$extView = $('#ext-view');
 	$intView = $('#int-view');
-	$bf = $('.showcase-bf');
-	$af = $('.showcase-af');
+	$miscView = $('#misc-view');
+	$finView = $('#fin-view');
+	$ext_bf = $('#ext-showcase-bf');
+	$ext_af = $('#ext-showcase-af');
+	$int_bf = $('#int-showcase-bf');
+	$int_af = $('#int-showcase-af');
+	$misc_bf= $('#misc-showcase-bf');
+	$misc_af= $('#misc-showcase-af');
+	$fin = $('#fin-showcase');
 
 	$('.ext-View').on('click', function () {
 
@@ -45,17 +52,120 @@ $(function() {
 		if ($extView.css('display') == ('none')) {
 			$extView.slideToggle(500);
 
-			$bf.html('<h3>Before</h3><img src=img/' + $id + '-bf.jpg>');
-			$af.html('<h3>After</h3><img src=img/' + $id + '-af.jpg>');
+			$ext_bf.html('<h3>Before</h3><img src=img/' + $id + '-bf.jpg>');
+			$ext_af.html('<h3>After</h3><img src=img/' + $id + '-af.jpg>');
 	
 		}	else	{		
-			$bf.html('<h3>Before</h3><img src=img/' + $id + '-bf.jpg>');
-			$af.html('<h3>After</h3><img src=img/' + $id + '-af.jpg>');
+			$ext_bf.html('<h3>Before</h3><img src=img/' + $id + '-bf.jpg>');
+			$ext_af.html('<h3>After</h3><img src=img/' + $id + '-af.jpg>');
 		};
 	});
 
 
+	$('.int-View').on('click', function () {
 
+			if ($(this).parent().hasClass('viewing')) {
+
+			$intView.slideUp(500);
+
+			};
+		});
+
+	$('.int-View').on('click', function () {
+
+		$id = $(this).attr('id');
+		$parent = $(this).parent();
+
+		if ($parent.siblings('.viewing')) {
+
+			$parent.siblings().removeClass('viewing');
+			$parent.addClass('viewing');
+
+		}	else {
+
+			$parent.addClass('viewing');
+		};
+
+		if ($intView.css('display') == ('none')) {
+			$intView.slideToggle(500);
+
+			$int_bf.html('<h3>Before</h3><img src=img/' + $id + '-bf.jpg>');
+			$int_af.html('<h3>After</h3><img src=img/' + $id + '-af.jpg>');
+	
+		}	else	{		
+			$int_bf.html('<h3>Before</h3><img src=img/' + $id + '-bf.jpg>');
+			$int_af.html('<h3>After</h3><img src=img/' + $id + '-af.jpg>');
+		};
+	});
+
+	$('.misc-View').on('click', function () {
+
+			if ($(this).parent().hasClass('viewing')) {
+
+			$miscView.slideUp(500);
+
+			};
+		});
+
+	$('.misc-View').on('click', function () {
+
+		$id = $(this).attr('id');
+		$parent = $(this).parent();
+
+		if ($parent.siblings('.viewing')) {
+
+			$parent.siblings().removeClass('viewing');
+			$parent.addClass('viewing');
+
+		}	else {
+
+			$parent.addClass('viewing');
+		};
+
+		if ($miscView.css('display') == ('none')) {
+			$miscView.slideToggle(500);
+
+			$misc_bf.html('<h3>Before</h3><img src=img/' + $id + '-bf.jpg>');
+			$misc_af.html('<h3>After</h3><img src=img/' + $id + '-af.jpg>');
+	
+		}	else	{		
+			$misc_bf.html('<h3>Before</h3><img src=img/' + $id + '-bf.jpg>');
+			$misc_af.html('<h3>After</h3><img src=img/' + $id + '-af.jpg>');
+		};
+	});
+
+	$('.fin-View').on('click', function () {
+
+			if ($(this).parent().hasClass('viewing')) {
+
+			$finView.slideUp(500);
+
+			};
+		});
+
+	$('.fin-View').on('click', function () {
+
+		$id = $(this).attr('id');
+		$parent = $(this).parent();
+
+		if ($parent.siblings('.viewing')) {
+
+			$parent.siblings().removeClass('viewing');
+			$parent.addClass('viewing');
+
+		}	else {
+
+			$parent.addClass('viewing');
+		};
+
+		if ($finView.css('display') == ('none')) {
+			$finView.slideToggle(500);
+
+			$fin.html('<img src=img/' + $id + '.jpg>');
+		}	else	{		
+			$fin.html('<img src=img/' + $id + '.jpg>');
+		};
+	});
 
 /***************Footer to Bottom***************/
 	
